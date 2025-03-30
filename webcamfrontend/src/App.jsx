3b91303545be093ebb20691181310c6e6b1464a5
Vitor Wakamatsu <vitorwak2@gmail.com>
2025-03-30 01:20:45 -0700

    Cloud Vision API working :)

--- webcamfrontend/src/App.jsx
+++ webcamfrontend/src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import Webcam from 'react-webcam'
 import WebCamCapture from './components/WebCamCapture';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <WebCamCapture />
