d35527d6067d707e132f0b3be35a58ac1cb8df84
Vitor Wakamatsu <vitorwak2@gmail.com>
2025-03-30 03:11:42 -0700

    Fixed aggregate error

--- webcamfrontend/vite.config.js
+++ webcamfrontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api": {
-        target:"http://localhost:5000", //shorthand
+        target:"http://localhost:5050", //shorthand
         changeOrigin: true
       }
     }
