a01ded4c77fe0c873783a99e1f6f5d7191d5fc16
charrmint <rbcalawagan.ca@gmail.com>
2025-03-29 20:23:39 -0700

    Added some functionalities for refrigerator

--- backend/routes/refrigeratorRoutes.js
+++ backend/routes/refrigeratorRoutes.js
@@ -27,4 +27,69 @@ router.post('/', async (req, res) => {
     }
 });
 
+// to add assign user, and put food
+
+// get refrigerator, assigned users, and assigned foods
+router.get('/:id', async (req, res) => {
+    try {
+        const refrigerator = await Refrigerator.findById(req.params.id)
+        .populate('users')
+        .populate('foods');
+
+        if (!refrigerator) {
+            return res.status(404).json({ message: 'Refrigerator not found'});
+        }
+
+        res.status(200).json(refrigerator);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+// remove user
+router.remUser('/:id/removeUser', async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const { id } = req.params;
+        
+        const refrigerator = await Refrigerator.findById(id);
+
+        if (!refrigerator) {
+            return res.status(404).json({ message: 'Refrigerator not found' });
+        }
+
+        refrigerator.users = refrigerator.users.filter(user => user.toString() !== userId);
+
+        await refrigerator.save();
+
+        res.status(200).json({ message: 'User removed succesfully', refrigerator });
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+});
+
+// remove foods
+router.remFoods('/:id/removeFoods', async (req, res) => {
+    try {
+        const { foodIds } = req.body;
+        const { id } = req.params;
+
+        const refrigerator = await Refrigerator.findById(id);
+
+        if (!refrigerator) {
+            return res.status(404).json({ message: 'Refrigerator not found' });
+        }
+
+        refrigerator.foodList = refrigerator.foodList.filter(food => !foodIds.includes(food.toString()));
+
+        await refrigerator.save();
+
+        res.status(200).json({ message: 'Foods removed succesfully', refrigerator });
+    } catch(err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+)
+
 export default router;
